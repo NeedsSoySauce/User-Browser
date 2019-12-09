db105564eff6894e96fc6bb8745a41f9008cbf79
NeedsSoySauce <feras.albaroudi@gmail.com>
2019-12-09 20:23:23 +1300

    Added components to HomePage

--- user-browser/src/App.tsx
+++ user-browser/src/App.tsx
@@ -2,13 +2,27 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import { HomePage } from './pages/HomePage';
 import { NoMatchPage } from './pages/NoMatchPage';
+import { ThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+
+const theme = createMuiTheme({
+  overrides: {
+    MuiPaper: {
+      root: {
+        padding: '10px',
+        marginBottom: '10px',
+      },
+    },
+  },
+});
 
 const App: React.FC = () => {
   return (
-    <Switch>
-      <Route exact path="/" component={HomePage} />
-      <Route component={NoMatchPage} />
-    </Switch>
+    <ThemeProvider theme={theme}>
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route component={NoMatchPage} />
+      </Switch>
+    </ThemeProvider>
   );
 }
 
--- user-browser/src/pages/HomePage.tsx
+++ user-browser/src/pages/HomePage.tsx
@@ -1,9 +1,59 @@
 import React from 'react';
+import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
+import { CssBaseline, Container, Grid, Paper, Typography, List, ListItem, ListItemIcon, ListItemText } from '@material-ui/core';
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    container: {
+      marginTop: theme.spacing(1)
+    },
+    paper: {
+        maxHeight: '95vh',
+        overflowY: "scroll",
+        overflowX: "hidden"
+    }
+  }),
+);
 
 export const HomePage: React.FC = () => {
+    const classes = useStyles();
+
     return (
-        <div>
-            Home
-        </div>
+        <CssBaseline>
+            <Container maxWidth="xl" className={classes.container}>
+                <Grid container spacing={2}>
+                    <Grid item xs={2}>
+                        <Grid container direction="column" spacing={1}>
+                            <Grid item>
+                                <Paper>
+                                    <Typography variant="h6">
+                                            User Browser
+                                    </Typography>
+                                </Paper>
+                            </Grid>
+                            <Grid item>
+                                <Paper className={classes.paper} style={{padding: 0}}>
+                                    <List style={{padding: 0}}>
+                                        <ListItem
+                                            button
+                                            selected={true}
+                                        >
+                                            <ListItemText primary="ListItem" />
+                                        </ListItem>
+                                    </List>
+                                </Paper>
+                            </Grid>
+                        </Grid>
+                    </Grid>
+                    <Grid item xs>
+                        <Paper className={classes.paper}>
+                        <Typography variant="body1">
+                                Details
+                            </Typography>
+                        </Paper>
+                    </Grid>
+                </Grid>
+            </Container>
+        </CssBaseline>
     );
 }
