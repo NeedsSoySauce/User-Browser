07b96844d1b657151b76aee3dd93577ae2bfd352
NeedsSoySauce <feras.albaroudi@gmail.com>
2019-12-11 13:42:43 +1300

    Display error if no users were loaded

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
-import { CssBaseline, Container, Grid, Paper, Typography, withWidth, CircularProgress } from '@material-ui/core';
+import { CssBaseline, Container, Grid, Paper, Typography, withWidth, CircularProgress, Button } from '@material-ui/core';
 import UserDetails from ".././components/UserDetails";
 import UsersList from ".././components/UsersList";
 import localforage from 'localforage';
+import ErrorIcon from '@material-ui/icons/Error';
 
 export interface IUser {
     cell: string,
@@ -59,22 +60,18 @@ export interface IUser {
 }
 
 async function storeUsers(users: IUser[]) {
-    console.log("storing")
-    // Store each user under a key by their uuid
+    // Store each user using their uuid as the key (this helps to prevent us from storing duplicates of the same user)
     users.forEach(user => {
         localUserStore.setItem(user.login.uuid, user);
     })
 }
 
-async function loadUsers(amount: number) {
+async function loadUsers() {
     let users: IUser[] = [];
-    await localUserStore.iterate((value: IUser, key: string, iterationNumber: number) => {
-        users.push(value);
-        // Stop iteration once we've got enough users
-        if (iterationNumber > amount) {
-            return null;
-        }
-    })
+    // **Note: The curly braces in the body of the arrow function below are required 
+    // otherwise this function will fail when loading an indexedDB of 5000+ users
+    // (for a reason I am yet to figure out!)
+    await localUserStore.iterate((user: IUser) => { users.push(user) });
     return users;
 }
 
@@ -90,6 +87,10 @@ const useStyles = makeStyles((theme: Theme) =>
         container: {
             marginTop: theme.spacing(1)
         },
+        loadingContainer: {
+            height: "50vh", 
+            textAlign: "center"
+        },
         listContainer: {
             padding: 0,
             [theme.breakpoints.down('xs')]: {
@@ -101,7 +102,10 @@ const useStyles = makeStyles((theme: Theme) =>
             overflowY: "auto",
             overflowX: "hidden"
         },
-
+        errorIcon: {
+            height: "40px",
+            width: "40px"
+        }
     }),
 );
 
@@ -113,43 +117,68 @@ const localUserStore = localforage.createInstance({
 const HomePage: React.FC<{ width: string }> = ({ width }) => {
     const classes = useStyles();
     const [pageNumber, setPageNumber] = useState(1);
+    const [results, setResults] = useState(25);
     const [users, setUsers] = useState<IUser[]>([]);
     const [selectedUser, setSelectedUser] = useState({});
+    const [usersLoaded, setUsersLoaded] = useState(false);
 
     // Fetch users and trigger a state update when complete, also triggers when the pageNumber is changed
+    // This function will prefer using fresh data returned from randomuser.me, but if it's unable to do this (e.g. if we're offline)
+    // it will fallback to using the indexedDB of users
     useEffect(() => {
         const abortController = new AbortController()
 
-        fetch(`https://randomuser.me/api/?page=${pageNumber}&results=25&seed=abc123`, { signal: abortController.signal })
-            .then(res => res.json())
+        fetch(`https://randomuser.me/api/?page=1&results=5000&seed=abc123&exc=id,registered`, { signal: abortController.signal })
+            .then(res => {
+                if (!res.ok) {
+                    throw Error(`${res.status} - ${res.statusText}`);
+                }
+                return res.json()
+            })
             .then(data => {
+                storeUsers(data.results);
                 setUsers(data.results);
-                storeUsers(data.results)
+                setUsersLoaded(true);
             })
             .catch((error) => {
                 if (error.code !== 20) { // AbortError "The user aborted a request."
-                    console.log('Error', { code: error.code, message: error.message, name: error.name })
+                    console.log(error)
                 }
 
                 // Fallback to using local database
-                loadUsers(25).then(setUsers)
+                loadUsers().then(users => {
+                    setUsers(users);
+                    setUsersLoaded(true);
+                })
             })
-
         return () => abortController.abort()
-    }, [pageNumber])
+    }, [pageNumber, results, usersLoaded])
 
+    // Display a progress indicator if users are still loading, otherwise display the users as a list
     let content = null
-    if (users.length === 0) {
+    if (!usersLoaded || (usersLoaded && users.length === 0)) {
         content = (
-            <Grid container justify="center" alignItems="center" direction="column" spacing={2} style={{ height: "50vh" }}>
+            <Grid container justify="center" alignItems="center" direction="column" spacing={2} className={classes.loadingContainer}>
                 <Grid item>
-                    <CircularProgress />
+                    { !usersLoaded ? <CircularProgress /> : <ErrorIcon className={classes.errorIcon} />}
                 </Grid>
                 <Grid item>
                     <Typography variant="caption">
-                        Fetching data...
+                        { !usersLoaded ? "Fetching data..." : "Failed to fetch data and no users are available in your local database."}
                     </Typography>
                 </Grid>
+                {!usersLoaded ? null : (
+                    <Grid item>
+                        <Button
+                            variant="outlined"
+                            onClick={() => {
+                                setUsersLoaded(false);
+                            }}
+                        >
+                            Fetch data
+                        </Button>
+                    </Grid>
+                )}
             </Grid>
         )
     } else {
@@ -158,7 +187,7 @@ const HomePage: React.FC<{ width: string }> = ({ width }) => {
             <Grid container spacing={2} direction={width === "xs" ? "column" : "row"}>
                 <Grid item md={3} sm={4}>
                     <Paper className={classes.listContainer}>
-                        <UsersList users={users} onChange={setSelectedUser} />
+                        <UsersList users={users} onChange={setSelectedUser} pageNumber={pageNumber} results={results} />
                     </Paper>
                 </Grid>
 
