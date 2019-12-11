7101ef461e05c1df7698008bebca3dda767ccebd
NeedsSoySauce <feras.albaroudi@gmail.com>
2019-12-11 13:09:24 +1300

    Paginate UsersList

--- src/components/UsersList.tsx
+++ src/components/UsersList.tsx
@@ -3,14 +3,37 @@ import { List, ListItem, ListItemAvatar, ListItemText, Avatar } from '@material-
 import { IUser } from '.././pages/HomePage';
 import { AppContext } from '../App';
 
-const UsersList: React.FC<{ users: Array<IUser>, onChange: (value: IUser) => any }> = ({ users, onChange }) => {
+interface IUsersListProps {
+    users: Array<IUser>, 
+    onChange: (value: IUser) => any, 
+    pageNumber: number, 
+    results: number
+}
+
+// Page numbers start from 1
+const UsersList: React.FC<IUsersListProps> = ({ users, onChange, pageNumber, results }) => {
     const [selectedUser, setSelectedUser] = useState({});
     const { searchbarValue, filterOptions, sortingOptions } = useContext(AppContext);
 
     let { gender, ageRange, countries } = filterOptions;
     let countryNames: string[] = countries.map((value: any) => value.name.toLowerCase());
 
-    // Sort users based on the selected ordering and direction
+    // Filter users by the current searchbar value and filter options and then create a list item for each of them
+    let matchingUsers = users
+        .filter((user) => {
+            let age = Number.parseInt(user.dob.age);
+
+            // If a user doesn't meet any of the filter requirements return false
+            if (!`${user.name.title} ${user.name.first} ${user.name.last}`.toLowerCase().includes(searchbarValue.toLowerCase()) ||
+                (gender !== "all" && user.gender !== filterOptions.gender) ||
+                age < ageRange[0] || age > ageRange[1] ||
+                (countryNames.length !== 0 && !countryNames.includes(user.location.country.toLowerCase()))) {
+                return false
+            }
+            return true;
+        })
+
+    // Sort matching users based on the selected ordering and direction
     let predicate = (a: IUser, b: IUser) => a.name.first.localeCompare(b.name.first);
     switch (sortingOptions.ordering) {
         case "Last name":
@@ -24,47 +47,53 @@ const UsersList: React.FC<{ users: Array<IUser>, onChange: (value: IUser) => any
             break;
     }
 
-    users.sort(predicate)
+    matchingUsers.sort(predicate)
 
     if (sortingOptions.direction === "Ascending") {
-        users.reverse();
+        matchingUsers.reverse();
     }
 
-    // Filter users by the current searchbar value and filter options and then create a list item for each of them
-    let items = users
-        .filter((user) => {
-            let age = Number.parseInt(user.dob.age);
+    let startingIndex: number;
 
-            // If a user doesn't meet any of the filter requirements return false
-            if (!`${user.name.title} ${user.name.first} ${user.name.last}`.toLowerCase().includes(searchbarValue.toLowerCase()) ||
-                (gender !== "all" && user.gender !== filterOptions.gender) ||
-                age < ageRange[0] || age > ageRange[1] ||
-                (countryNames.length !== 0 && !countryNames.includes(user.location.country.toLowerCase()))) {
-                return false
-            }
-            return true;
-        })
-        .map((user) => {
-            return (
-                <ListItem
-                    key={user.login.uuid}
-                    button
-                    selected={selectedUser === user}
-                    onClick={e => {
-                        setSelectedUser(user);
-                        onChange(user);
-                    }}
-                >
-                    <ListItemAvatar>
-                        <Avatar alt={`${user.name.title} ${user.name.first} ${user.name.last}`} src={user.picture.thumbnail} />
-                    </ListItemAvatar>
-                    <ListItemText
-                        primary={`${user.name.first} ${user.name.last}`}
-                        secondary={`${user.dob.age}, ${user.gender === "male" ? "Male" : "Female"}, ${user.nat}`}
-                    />
-                </ListItem>
-            )
-        })
+    // If the page number is less than 1 we display the first page
+    if (pageNumber < 1) { 
+        startingIndex = 0;
+    // If the page number is too large we display the last page
+    } else if (pageNumber > Math.ceil(matchingUsers.length / results)) { 
+        startingIndex = results * (Math.ceil(matchingUsers.length / results) - 1);
+    } else {
+        startingIndex = results * (pageNumber - 1);
+    }
+
+    // Create list items for the matching users
+    let items = [];
+    for (let i = startingIndex; i < startingIndex + results; i++) {
+
+        if (matchingUsers[i] === undefined) {
+            break;
+        }
+
+        let user = matchingUsers[i];
+        items.push(
+            <ListItem
+                key={user.login.uuid}
+                button
+                selected={selectedUser === user}
+                onClick={e => {
+                    setSelectedUser(user);
+                    onChange(user);
+                }}
+            >
+                <ListItemAvatar>
+                    <Avatar alt={`${user.name.title} ${user.name.first} ${user.name.last}`} src={user.picture.thumbnail} />
+                </ListItemAvatar>
+                <ListItemText
+                    primary={`${user.name.first} ${user.name.last}`}
+                    secondary={`${user.dob.age}, ${user.gender === "male" ? "Male" : "Female"}, ${user.nat}`}
+                />
+            </ListItem>
+        )
+    }
 
     return (
         <List style={{ padding: 0 }}>
